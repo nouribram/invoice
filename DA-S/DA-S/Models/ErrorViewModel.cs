7736aa4b2bbb966b533572b8aa26c7503dcc715e
nouribram <nourtesla@yahoo.com>
2025-06-05 23:05:52 +0300

    new

---
+++ DA-S/DA-S/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DA_S.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
